14be0182215d6537f4756c10f3a6c3730ae5be6e
isaacmartinez3133-dot <isaacmartinez3133@gmail.com>
2026-04-21 13:21:53 -0600

    Create Tarea2Ejercicio1.c

---
+++ Tarea2Ejercicio1.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+int main ()
+{
+  int a, b, c, d, f;
+  printf("Ingresa el valor de a: ");  
+  printf("Ingresa el valor de b: ");
+  scanf("%d", &a);
+  scanf("%d", &b);
+  c = a + b;
+  d = a - b;
+  f = a * b;
+  return 0;
+}
